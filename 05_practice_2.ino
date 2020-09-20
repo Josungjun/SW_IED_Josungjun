fdb2660f3c85546b02cf6dd225605af2871f2b7c
Josungjun <66383669+Josungjun@users.noreply.github.com>
2020-09-20 22:04:30 +0900

    Create 05_practice_2.ino
    05_practice_2.ino 불을 1초간 키고 1초내에 5번 깜빡이게 한 뒤 종료 후 무한루프

---
+++ 05_practice_2.ino
@@ -0,0 +1,25 @@
+#define PIN_LED 7
+unsigned int toggle, count, second;
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  count = 0;
+  toggle = 1;
+  second = 1000;
+}
+
+void loop() {
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  delay(second);
+  second = 100;
+  count++;
+  if (count == 12) {
+    while(1){
+      
+    }
+  }
+}
+
+int toggle_state(int toggle) {
+  return 1 - toggle;
+}
